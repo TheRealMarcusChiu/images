9d26a584b3c17d150b117d16bf36d63f1fc76672
TheRealMarcusChiu <marcuschiu9@gmail.com>
2026-06-12 12:39:39 -0500

    admin: update tile 2026-06-09T23:51:28.050-05:00

--- content/items.js
+++ content/items.js
@@ -45,8 +45,7 @@ window.GALLERY_ITEMS = [
   { type: "image", date: "2026-06-10T01:01:17.463-05:00", file: "2026-06-10T01:01:17.463-05:00.png",
     link: "http://git.marcuschiu.com/var-log/enter-visual/art/draw-random-girl/", hidden: true },
 
-  { type: "image", date: "2026-06-09T23:51:28.050-05:00", file: "2026-06-09T23:51:28.050-05:00.webp",
-    link: "https://www.youtube.com/watch?v=xa-4IAR_9Yw" },
+  { type: "image", date: "2026-06-09T23:51:28.050-05:00", file: "2026-06-09T23:51:28.050-05:00.webp" },
 
   { type: "image", date: "2026-06-09T20:38:35.308-05:00", file: "2026-06-09T20:38:35.308-05:00.webp",
     desc: "the most personal is the most creative", link: "https://archive.org/details/MereChristianityCSL/page/n125/mode/2up#:~:text=you%20will%20never,find%20eternal%20life." },
