57b0fd8b2b0768a8049247e8a8ee745e9c057f57
root <root@my-websites-server.lan>
2026-08-28 15:03:59 -0500

    admin: update 2026-08-28T20:03:59.005Z

--- content/items.js
+++ content/items.js
@@ -17,6 +17,9 @@
      poster filename in content/media/ — video thumbnail or audio cover (optional)
    ────────────────────────────────────────────────────────────────────────── */
 window.GALLERY_ITEMS = [
+  { type: "quote", date: "2026-08-28T15:03:54.741-05:00",
+    quote: "I wonder what's it like to be Uriah in 2 Samuel 11–12 - then hear that David is most often referred to as a man after God's heart.", author: "2 Samuel 11–12" },
+
   { type: "youtube", date: "2026-08-27T23:26:55.625-05:00", id: "JF0U5z5XA9M",
     desc: "See quote on the Summary of Proverbs" },
 
